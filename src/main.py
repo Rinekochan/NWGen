a3f771f0c143f9e3666b537b443f5b762abf69b8
Hoang <viethoang9314@gmail.com>
2025-03-02 22:25:17 +1100

    Add html template and copy functionality for static files

--- src/main.py
+++ src/main.py
@@ -1,10 +1,20 @@
-﻿from src.utils import extract_markdown_images
-from textnode import TextNode, TextType
+﻿import os
+import shutil
+
+from copystatic import copy_files_recursive
+
+
+dir_path_static = "../static"
+dir_path_public = "../public"
 
 
 def main():
-    text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
-    print(extract_markdown_images(text))
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
 
 
 main()
--- src/textnode.py
+++ src/textnode.py
@@ -32,7 +32,7 @@ def text_node_to_html_node(text_node: TextNode) -> HTMLNode:
             return LeafNode("b", text_node.text)
         case TextType.ITALIC:
             return LeafNode("i", text_node.text)
-        case TextType.CODE_TEXT:
+        case TextType.CODE:
             return LeafNode("code", text_node.text)
         case TextType.LINK:
             return LeafNode("a", text_node.url, dict({"href": f"{text_node.url}"}))
